7f2eeea4fe41d690f851c2f9dc0a7600a006697c
Felix <Felix.Wedlich@gmail.com>
2022-10-11 13:52:15 +0200

    expose reuseAnalyzer through binding

--- reusedist/backend/reusedist_wrapper.cpp
+++ reusedist/backend/reusedist_wrapper.cpp
@@ -1,12 +1,18 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 
-#include "reusedist.h"
+#include "reuseAnalyzer.h"
+
 namespace py = pybind11;
 
-//TODO comment why
+// TODO comment why
 py::object return_none() { return py::cast<py::none>(Py_None); }
 
-PYBIND11_MODULE(reusedist, m){
-    py::class_<reusedist,std::shared_ptr<reusedist>>(m,"reusedist").def(py::init<>());
+PYBIND11_MODULE(reusedist, m) {
+  py::class_<reuseAnalyzer, std::shared_ptr<reuseAnalyzer>>(m, "ReuseAnalyzer")
+      .def(py::init<int, int, int, int>(), py::arg("sets"), py::arg("ways"),
+           py::arg("cacheline_size"), py::arg("block_size"))
+      .def("process_load", &reuseAnalyzer::processLoad)
+      .def("process_store", &reuseAnalyzer::processStore)
+      .def("get_reuse_distance_counts", &reuseAnalyzer::getReuseDistanceCounts);
 }
