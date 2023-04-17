128b6db29c029c42544811a92bc3595cf18ea07c
Konstantin Lübeck <konstantin.luebeck@uni-tuebingen.de>
2023-04-17 08:34:34 +0200

    Renamed tests directory to test.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 from reuse_distance_analyzer import ReuseDistanceAnalyzer
 
 rda = ReuseDistanceAnalyzer(4, 8, 64, 16)
+
+rda.process_load(1024)
--- reuse_distance_analyzer/backend/reuse_distance_analyzer_wrapper.cc
+++ reuse_distance_analyzer/backend/reuse_distance_analyzer_wrapper.cc
@@ -10,13 +10,8 @@ PYBIND11_MODULE(pybind_reuse_distance_analyzer, m) {
 
     py::class_<ReuseDistanceAnalyzer>(m, "ReuseDistanceAnalyzer")
         .def(py::init<uint32_t, uint32_t, uint32_t, u_int32_t>(), py::arg("sets"), py::arg("ways"), py::arg("cache_line_size"),
-             py::arg("block_size"));
-
-    /* py::class_<reuseAnalyzer, std::shared_ptr<reuseAnalyzer>>(m, "ReuseAnalyzer")
-        .def(py::init<int, int, int, int>(), py::arg("sets"), py::arg("ways"), py::arg("cacheline_size"), py::arg("block_size"))
-        .def("process_load", &reuseAnalyzer::processLoad)
-        .def("process_store", &reuseAnalyzer::processStore)
-        .def("get_reuse_distance_counts", &reuseAnalyzer::getReuseDistanceCounts)
-        .def(py::init<std::shared_ptr<SetAssociativeCache>, int>(), py::arg("set_associative_cache"), py::arg("block_size") = 16)
-        .def("analyze_instruction_generator", &reuseAnalyzer::analyzeInstructionGenerator, py::arg("instruction_generator"));
- */}
+             py::arg("block_size"))
+        .def("process_load", &ReuseDistanceAnalyzer::process_load)
+        .def("process_load", &ReuseDistanceAnalyzer::process_store)
+        .def("get_reuse_distance_counts", &ReuseDistanceAnalyzer::get_reuse_distance_counts);
+}
